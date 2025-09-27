cac54e18260f1906bada05a96b3af3ae0ef4bf61
sri shakthi <srishakthi32@gmail.com>
2025-09-28 00:52:59 +0530

    lllll

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './' // <-- makes all assets relative so it works on any path
 })
