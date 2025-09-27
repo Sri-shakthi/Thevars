3b74ded4be0f5d2fe4016bd4e77bf35d70a413ed
sri shakthi <srishakthi32@gmail.com>
2025-09-28 01:05:06 +0530

    im

--- frontend/src/components/HomePage/index.jsx
+++ frontend/src/components/HomePage/index.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import { motion } from "framer-motion";
 import "./index.css";
+import hoo from '../../assets/hoo.png'
 
 export default function HomePage() {
   return (
-    <div className="homepage">
+    <div className="homepage"
+    style={{ backgroundImage: `url(${hoo})` }}
+    >
       <div className="navbar">
         <p>About Us</p>
         <p>Our Menu</p>
